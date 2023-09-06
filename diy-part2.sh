d269cd5174cf9b1889f8d4df3499ef3c08c7eaec
wvvwcom <wvvwcom@qq.com>
2023-09-06 16:47:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,8 @@
 # ./scripts/feeds install -p nuexini alist luci-app-alist luci-app-vlmcsd
 
 # 更新golang版本，alist编译要求20.x
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 
 # 删除多余的主题
 # sed -i '/CONFIG_PACKAGE_luci-theme-argon=y/d' .config
